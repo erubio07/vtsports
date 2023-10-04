2e180676eb241f5bb88d6998ca751b60cd13a26d
Ezequiel Rubio <erubio07@gmail.com>
2023-10-04 14:11:12 +0000

    fill genres, types, waist

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 const server = require("./src/app");
+const fillGenre = require("./src/controllers/fillGenres");
+const fillType = require("./src/controllers/fillType");
 
 const { conn } = require("./src/db.js");
 const port = 3001;
@@ -6,5 +8,7 @@ const port = 3001;
 conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
     console.log(`Servidor iniciado en el puerto ${port}`);
+    fillGenre();
+    fillType();
   });
 });
