1225ae84e6a60b0b4fe7564e44dc335b550b75b4
Ezequiel Rubio <erubio07@gmail.com>
2023-10-06 16:39:49 +0000

    db deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_DEPLOY } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://erubio07:DJ9z3GFvhVdO@ep-small-poetry-57455562.us-east-2.aws.neon.tech/neondb?sslmode=require`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
