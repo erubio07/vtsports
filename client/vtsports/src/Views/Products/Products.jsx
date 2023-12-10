82f8464fee688a7e6d85787371f6504b00ac9a46
Ezequiel Rubio <erubio07@gmail.com>
2023-12-10 08:28:23 -0300

    estilos en las tarjetas

--- client/vtsports/src/Views/Products/Products.jsx
+++ client/vtsports/src/Views/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Products.module.css";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { getAllProducts } from "../../Redux/actions";
 
 const Products = () => {
@@ -10,27 +11,33 @@ const Products = () => {
   const productsFilter = useSelector((state) => state.productsFilter);
   console.log(products);
   // console.log(productsFilter);
-
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(getAllProducts());
   }, [dispatch]);
 
   return (
     <div>
-      {productsFilter.map((p) => (
-        <div key={p.id}>
-          <img src={p.image} alt={p.name} />
-          <h2>{p.name}</h2>
-          <p>{p.description}</p>
-          <h3>Material:</h3>
-          <p>{p.Type.name}</p>
-          <h3>Talles:</h3>
-          {p.Waists.map((w) => {
-            return <p>{w.name}</p>;
-          })}
-          <p>{p.price}</p>
-        </div>
-      ))}
+      <div className={styles.container}>
+        {productsFilter.map((p) => (
+          <div key={p.id} className={styles.productCard}>
+            <img src={p.image} alt={p.name} className={styles.productImage} />
+            <h2 className={styles.productName}>{p.name}</h2>
+            {/* <p className={styles.productDescription}>{p.description}</p> */}
+            <div className={styles.productDetails}>
+              <p>
+                <strong>Material:</strong> {p.Type.name}
+              </p>
+
+              <p>
+                <strong>Talles:</strong>{" "}
+                {p.Waists.map((w) => w.name).join(", ")}
+              </p>
+            </div>
+            <p className={styles.productPrice}>$ {p.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
