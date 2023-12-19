98ca84b01205fc71ce9f1faca58096d2c2a63ca2
maxilucero <maxiluceroymish9@gmail.com>
2023-12-19 13:12:25 -0300

    cambios

--- client/vtsports/src/Views/Products/Products.jsx
+++ client/vtsports/src/Views/Products/Products.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Products.module.css";
 
 const Products = () => {
-  return <div>Products</div>;
+  return <div className={styles.products}>Products</div>;
 };
 
 export default Products;
