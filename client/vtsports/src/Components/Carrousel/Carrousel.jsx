6bb5422df48be1d6a1ddffa4d8d1af4a1dfee2bd
Ezequiel Rubio <erubio07@gmail.com>
2023-12-28 00:19:18 -0300

    pase los estilos al carrousel

--- client/vtsports/src/Components/Carrousel/Carrousel.jsx
+++ client/vtsports/src/Components/Carrousel/Carrousel.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import styles from "./Carrousel.module.css";
 
 const Carrousel = ({ randomProducts }) => {
   console.log(randomProducts);
@@ -16,13 +17,17 @@ const Carrousel = ({ randomProducts }) => {
     cssEase: "linear",
   };
   return (
-    <div>
+    <div className={styles.carrouselContainer}>
       <Slider {...settings}>
         {randomProducts.map((p) => (
-          <div key={p.id}>
-            <img src={p.image} alt={p.name} />
-            <h4>{p.name}</h4>
-            <p>{p.price}</p>
+          <div key={p.id} className={styles.productContainer}>
+            <div className={styles.imageContainer}>
+              <img src={p.image} alt={p.name} className={styles.productImage} />
+            </div>
+            <div className={styles.productDetails}>
+              <h4 className={styles.productTitle}>{p.name}</h4>
+              <p className={styles.productPrice}>{p.price}</p>
+            </div>
           </div>
         ))}
       </Slider>
