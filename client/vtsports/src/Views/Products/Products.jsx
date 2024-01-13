6b12932273a977db1670fd141029a918e80cf33d
Ezequiel Rubio <erubio07@gmail.com>
2024-01-13 09:46:23 -0300

    tarjetas y modal de boostrap

--- client/vtsports/src/Views/Products/Products.jsx
+++ client/vtsports/src/Views/Products/Products.jsx
@@ -2,24 +2,40 @@ import React from "react";
 import styles from "./Products.module.css";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { getAllProducts } from "../../Redux/actions";
 import Pagination from "../../Components/Pagination/Pagination";
 import ReactLoading from "react-loading";
 import HomeSLider from "../../Components/Slider/HomeSlider";
 import Filter from "../../Components/Filter/Filter";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
 const Products = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products);
   const productsFilter = useSelector((state) => state.productsFilter);
   console.log(products);
   // console.log(productsFilter);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const totalProducts = products.length;
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [itemsPerPage, setItemsPerPage] = useState(12);
   const [currentPage, setCurrentPage] = useState(1);
   const lastIndex = currentPage * itemsPerPage;
   const firstIndex = lastIndex - itemsPerPage;
+  const [show, setShow] = useState(false);
+  const [productModal, setProductModal] = useState(null);
+  console.log(productModal);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (p) => {
+    setProductModal(p);
+    setShow(true);
+  };
 
   useEffect(() => {
     dispatch(getAllProducts());
@@ -42,37 +58,86 @@ const Products = () => {
         ) : (
           productsFilter
             .map((p) => (
-              <div key={p.id} className={styles.productCard}>
-                <img
+              <Card style={{ width: "18rem", marginBottom: "30px" }}>
+                <Card.Img
+                  variant="top"
                   src={p.image}
-                  alt={p.name}
                   className={styles.productImage}
                 />
-                <h2 className={styles.productName}>{p && p.name}</h2>
-                {/* <p className={styles.productDescription}>{p.description}</p> */}
-                <div className={styles.productDetails}>
-                  <p>
-                    <strong>Material:</strong> {p && p.Type.name}
-                  </p>
-
-                  <p>
-                    <strong>Talles:</strong>{" "}
-                    {p.Waists.map((w) => w.name).join(", ")}
-                  </p>
-                </div>
-                <p className={styles.productPrice}>$ {p.price}</p>
-                <button
-                  className={styles.button}
-                  onClick={() => navigate(`/products/${p.id}`)}
+                <Card.Body>
+                  <Card.Title>{p.name}</Card.Title>
+                  <Card.Text>
+                    <p>
+                      <strong>Material:</strong> {p && p.Type.name}
+                    </p>
+                    <p>
+                      <strong>Talles:</strong>{" "}
+                      {p.Waists.map((w) => w.name).join(", ")}
+                    </p>
+                    <p className={styles.productPrice}>$ {p.price}</p>
+                  </Card.Text>
+                  <Button variant="primary" onClick={() => handleShow(p)}>
+                    Detalles
+                  </Button>
+                </Card.Body>
+                <Modal
+                  show={show}
+                  onHide={handleClose}
+                  aria-labelledby="contained-modal-title-vcenter"
+                  centered
                 >
-                  Detalles
-                </button>
-              </div>
+                  <Modal.Header closeButton>
+                    <Modal.Title>
+                      {productModal && productModal.name}
+                    </Modal.Title>
+                  </Modal.Header>
+
+                  <Modal.Body>
+                    <Container>
+                      <Row>
+                        <Col xs={6} md={4}>
+                          <Image
+                            src={productModal && productModal.image}
+                            rounded
+                            style={{ width: 171, height: 180 }}
+                          />
+                        </Col>
+                      </Row>
+                    </Container>
+                    <p style={{ marginTop: 15 }}>
+                      {productModal && productModal.description}
+                    </p>
+                    <p>
+                      <strong>Material:</strong>{" "}
+                      {productModal && productModal.Type.name}
+                    </p>
+                    <p>
+                      <strong>Talles:</strong>{" "}
+                      {productModal &&
+                        productModal.Waists.map((w) => w.name).join(", ")}
+                    </p>
+                    <p className={styles.productPrice}>
+                      $ {productModal && productModal.price}
+                    </p>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      Close
+                    </Button>
+                    <Link
+                      to="https://api.whatsapp.com/send?phone=543516237423"
+                      target="_blank"
+                    >
+                      <Button variant="primary">Contactar</Button>
+                    </Link>
+                  </Modal.Footer>
+                </Modal>
+              </Card>
             ))
             .slice(firstIndex, lastIndex)
         )}
       </div>
-      <div className={styles.paginacion}>
+      <div className={styles.paginacion} style={{ marginTop: "30px" }}>
         <Pagination
           itemsPerPage={itemsPerPage}
           currentPage={currentPage}
