6a40f4a9828225db2182f0f94a9db623e39d50f7
Ezequiel Rubio <erubio07@gmail.com>
2024-01-18 12:51:29 +0000

    corregido el error de codespace

--- api/src/app.js
+++ api/src/app.js
@@ -12,7 +12,7 @@ server.use(cors());
 server.use(morgan("dev"));
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ´${code}´); //Autorizo recibir solicitudes de este dominio
+  res.header("Access-Control-Allow-Origin", "https://turbo-yodel-wqwv465j54gc5xj4-3000.app.github.dev"); //Autorizo recibir solicitudes de este dominio
   res.header("Access-Control-Allow-Credentials", true); //Autorizo recibir solicitudes que incluyan el encabezado con credenciales
   res.header(
     "Access-Control-Allow-Headers",
