e9c5c9b661b92f80bc8b7cbef4564e93b6540b2d
Ezequiel Rubio <erubio07@gmail.com>
2024-01-19 20:30:44 +0000

    haciendo funcionar el dashboard

---
+++ client/vtsports/src/Components/ProductsAdmin/ProductsAdmin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProductsAdmin = () => {
+    return <div>
+        <h1> Products Admin </h1>
+    </div>
+    
+}
+
+export default ProductsAdmin;
--- client/vtsports/src/Views/Dashboard/Dashboard.jsx
+++ client/vtsports/src/Views/Dashboard/Dashboard.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import { Routes, Route, useLocation, BrowserRouter } from "react-router-dom";
 import styles from "./Dashboard.module.css";
+import { ProtectedRoutes } from "../../Components/ProtectedRoutes/ProtectedRoutes.jsx";
 import SideBar from "../../Components/SideBar/SideBar.jsx"
+import ProductsAdmin from "../../Components/ProductsAdmin/ProductsAdmin.jsx"
 
 const Dashboard = () => {
   return (
+    
+
     <div className={styles.dashboardContainer}>
       <SideBar/>
+      
       <div className={styles.content}>
+      <Routes>
 
-      <h1> Dashboard Protegido </h1>
+      <Route
+            path="/dashboard/products"
+            element={<ProtectedRoutes element={<ProductsAdmin />} />}
+          />
+        
+        </Routes>
       </div>
+      
     </div>
+    
   );
 };
 
