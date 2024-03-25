e6708e6de1e35b1f27ac30ff3324e15c935c7ef1
Ezequiel Rubio <erubio07@gmail.com>
2024-03-25 17:26:15 +0000

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ server.use(cors());
 server.use(morgan("dev"));
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); //Autorizo recibir solicitudes de este dominio
+  res.header("Access-Control-Allow-Origin", "https://vtsports-dev-dxex.3.us-1.fl0.io"); //Autorizo recibir solicitudes de este dominio
   res.header("Access-Control-Allow-Credentials", true); //Autorizo recibir solicitudes que incluyan el encabezado con credenciales
   res.header(
     "Access-Control-Allow-Headers",
