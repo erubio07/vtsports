92742c4f87f5903d3b3194598955038c0f73d87b
Ezequiel Rubio <erubio07@gmail.com>
2024-05-30 19:10:35 -0300

    Actualizar app.js

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ server.use(cors());
 server.use(morgan("dev"));
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://redesigned-capybara-q5wvxr4gp56h4rvr-3000.app.github.dev"); //Autorizo recibir solicitudes de este dominio
+  res.header("Access-Control-Allow-Origin", "https://localhost:3000"); //Autorizo recibir solicitudes de este dominio
   res.header("Access-Control-Allow-Credentials", true); //Autorizo recibir solicitudes que incluyan el encabezado con credenciales
   res.header(
     "Access-Control-Allow-Headers",
