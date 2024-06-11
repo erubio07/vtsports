7aadfe9ea58d7eb527f316c98a7bd10e5c1ce49a
Ezequiel Rubio <erubio07@gmail.com>
2024-06-11 20:39:09 +0000

    stilos featuredProducts

--- client/vtsports/src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ client/vtsports/src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import styles from "./FeaturedProducts.module.css"
 
 const FeaturedProducts = ({randomProducts}) => {
     console.log(randomProducts);
     return (
-        <div>
+        <div className={styles.container}>
         {randomProducts.map((p) => (
-            <div key={p.id}>
-            <div>
-            <img src={p.image} alt={p.name}/>
+            <div key={p.id} className={styles.card}>
+            <div className={styles.imageContainer}>
+            <img src={p.image} alt={p.name} className={styles.image}/>
             </div>
-            <div>
-            <h4>{p.name}</h4>
-            <p>$ {p.price}</p>
+            <div className={styles.info}>
+            <h4 className={styles.name}>{p.name}</h4>
+            <p className={styles.price}>$ {p.price}</p>
             </div>
             </div>
         ))}
