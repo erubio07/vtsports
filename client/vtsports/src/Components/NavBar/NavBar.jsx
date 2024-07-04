2dbb6c2b265333342df3e4e7f5d79fc8577f529e
Ezequiel Rubio <erubio07@gmail.com>
2024-07-04 20:25:08 +0000

    menuMobile ok

--- client/vtsports/src/Components/NavBar/NavBar.jsx
+++ client/vtsports/src/Components/NavBar/NavBar.jsx
@@ -2,48 +2,73 @@ import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getUserById } from "../../Redux/actions";
+import MenuMobile from "../MenuMobile/MenuMobile";
 import styles from "./NavBar.module.css";
+import { PiListBold } from "react-icons/pi";
+import { RxCross2 } from "react-icons/rx";
 
 function Navbar() {
   const user = useSelector((state) => state.user);
-  // console.log(user);
-  const dispacth = useDispatch();
-  const id = localStorage.getItem("userId")
-  // console.log(id);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+  const id = localStorage.getItem("userId");
+
+  const handleMenuMobile = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   useEffect(() => {
-    if(id){
-
-      dispacth(getUserById(id))
+    if (id) {
+      dispatch(getUserById(id));
     }
-  }, [id]);
+  }, [id, dispatch]);
 
   return (
     <div className={styles.container}>
-      <img
-        src="https://res.cloudinary.com/dytke2vlw/image/upload/v1701527722/vtsport/yb2owexc1s5bfmrqpd0c.png"
-        alt="logo VtSports"
-        className={styles.logo}
+      <div className={styles.mobileMenuButtonContainer}>
+        <img
+          src="https://res.cloudinary.com/dytke2vlw/image/upload/v1701527722/vtsport/yb2owexc1s5bfmrqpd0c.png"
+          alt="logo VtSports"
+          className={styles.logo}
+        />
+        {isMobileMenuOpen ? (
+          <RxCross2
+            className={styles.mobileMenuButton}
+            style={{ fontSize: "2rem" }}
+            color="white"
+            onClick={handleMenuMobile}
+          />
+        ) : (
+          <PiListBold
+            className={styles.mobileMenuButton}
+            style={{ fontSize: "2rem" }}
+            color="white"
+            onClick={handleMenuMobile}
+          />
+        )}
+      </div>
+      <MenuMobile
+        isOpen={isMobileMenuOpen}
+        handleMenuMobile={handleMenuMobile}
       />
-      {user.id && (
-        <NavLink className={styles.link} to="/dashboard">
-          Dashboard
+      <div className={`${styles.navLinks} ${isMobileMenuOpen ? styles.hide : ""}`}>
+        {user.id && (
+          <NavLink className={styles.link} to="/dashboard">
+            Dashboard
+          </NavLink>
+        )}
+        <NavLink className={styles.link} to="/">
+          Home
         </NavLink>
-      )}
-      <NavLink className={styles.link} to="/">
-        Home
-      </NavLink>
-      <NavLink className={styles.link} to="/products">
-        Productos
-      </NavLink>
-      {/* <NavLink className={styles.link} to="/about">
-        Sobre Nosotros
-      </NavLink> */}
-      {!user.id && (
-        <NavLink className={`${styles.link} ${styles['link-login']}`} to="/login">
-          Login
+        <NavLink className={styles.link} to="/products">
+          Productos
         </NavLink>
-      )}
+        {!user.id && (
+          <NavLink className={`${styles.link} ${styles["link-login"]}`} to="/login">
+            Login
+          </NavLink>
+        )}
+      </div>
     </div>
   );
 }
