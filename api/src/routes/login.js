2831879025d2c2b0633e6f371c5802056c699bfc
Ezequiel Rubio <erubio07@gmail.com>
2024-07-08 15:36:17 +0000

    separando el front

--- api/src/routes/login.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require("express");
-const {loginHandler} = require ("../handlers/loginHandlers.js");
-
-const router = Router();
-
-router.post("/", loginHandler);
-
-module.exports = router;
